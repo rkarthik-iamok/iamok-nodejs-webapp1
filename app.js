d503629cf54b85083c03a064c7002320582d2357
rkarthik-iamok <rkarthik.iamok@gmail.com>
2022-12-26 17:19:23 -0500

    Added Express

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log ("Hello, World!");
+// Import Express 
+const express = require('express');
+
+
+// Initialize Express module
+const app = express();
+
+// Routes 
+app.get('/', (req, res)=> {
+    res.send("Hello, World!");
+});
+
+// Listen for requests
+app.listen(8080, '0.0.0.0', ()=> {
+    console.log("Server is listening on PORT 8080...");
+});
